51566ddbeb705f03e1b72636f8d9e4325a3098d9
dlxkhang <khangduong0902@gmail.com>
2022-04-16 14:44:53 +0700

    charts for home page

--- source/routes/home.js
+++ source/routes/home.js
@@ -5,4 +5,7 @@ const homeController = require('../controllers/homeController');
 
 router.get('/', homeController.home);
 
+router.get('/home-tour-overview/:filter', homeController.loadHomeTourByFilter);
+router.get('/properties-of-interest', homeController.loadPropertiesOfInterest);
+router.get('/top-10-properties-of-interest', homeController.loadTop10PropertiesOfInterest);
 module.exports = router;
