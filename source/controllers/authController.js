df923f225d1ef692de1765f78bec68fa0ddd1735
Huynh Ba Huy <19127420@student.hcmus.edu.vn>
2022-04-28 20:51:40 +0700

    minor changes

--- source/controllers/authController.js
+++ source/controllers/authController.js
@@ -8,11 +8,7 @@ class authController {
         // res.render('login',{layout:false, wrongPassword: wrongPassword});
     }
 
-    //[POST] /login/change-password
-    async changePassword(req, res) {
-        const ack = await adminService.changePassword(req.body.username, req.body.password,req.body.newPassword);
-        res.redirect('/profile?'+ack);
-    }
+    
 }
 
 module.exports = new authController();
